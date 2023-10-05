914c2cb9e29f0fe5418b8c07739fbbaf64c5b522
Pratik khulge <113931760+pratikkhulge@users.noreply.github.com>
2023-10-05 22:48:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
